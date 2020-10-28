021b46f09e316d424b19c283fd0a52b5df72665d
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-10-27 21:35:19 -0500

    Add test class for products repository

---
+++ tests/CoffeeShop.Tests/Repositories/ProductsRepositoryTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CoffeeShop.Tests.Repositories
+{
+    public class ProductsRepositoryTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
